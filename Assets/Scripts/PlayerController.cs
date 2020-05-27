15cfecc9bf12d0a3d7683863266a717dbf352f80
Daniel Ilett <danielilett@gmail.com>
2020-05-27 15:51:25 +0100

    Initial commit. Basic character movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Vector2 moveVector;
+
+    private new Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        moveVector = new Vector2(x, y);
+
+        if(moveVector.magnitude > 1.0f)
+        {
+            moveVector.Normalize();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = moveVector * speed;
+    }
+}
